aad4cd25bb20ca66b48d56911a2efa4433167171
Maksim Kochkin <maksim.kochkin@corp.badoo.com>
2019-08-20 23:38:24 +0300

    add preview

--- todo.php
+++ todo.php
@@ -16,16 +16,16 @@ $help = <<<'HELP'
 
 Usage: php todo.php [command] [options]
 
-  php todo.php                                                     - show all tasks
-  php todo.php add <task text> --do-till=<datetime>                - add new task
-  php todo.php edit <task id> <new text> --do-till=<new datetime>  - edit task
-  php todo.php del <task id>                                       - remove task
-  php todo.php -i                                                  - interactive mode
-  php todo.php --help                                              - show this help 
+  php todo.php                                                      - show all tasks
+  php todo.php add <task text> --deadline=<datetime>                - add new task
+  php todo.php edit <task id> <new text> --deadline=<new datetime>  - edit task
+  php todo.php del <task id>                                        - remove task
+  php todo.php -i                                                   - interactive mode
+  php todo.php --help                                               - show this help 
 
 HELP;
 
-$options = getopt('', ['help']); // https://www.php.net/manual/ru/function.getopt.php
+$options = getopt('', ['help', 'deadline:'], $optind); // https://www.php.net/manual/ru/function.getopt.php
 if (isset($options['help'])) {
     echo $help;
     exit;
@@ -33,14 +33,27 @@ if (isset($options['help'])) {
 
 // var_dump($argv);
 
-$command = $argv[1] ?? ''; // https://www.php.net/manual/ru/reserved.variables.argv.php
+$command = $argv[$optind] ?? ''; // https://www.php.net/manual/ru/reserved.variables.argv.php
 
 if ($command === '') {
     // This is 'show' command
     $tasks = read_tasks_from_file('db/tasks.db');
     echo render_todo_list($tasks);
 } elseif ($command === 'add') {
-    echo 'this is ADD command' . PHP_EOL;
+    $task_text = $argv[$optind + 1] ?? '';
+    // Validation.
+    if ($task_text === '') {
+        echo red_line('Please specify a text for "add" command');
+        echo $help;
+        exit(1);
+    }
+
+    $deadline = $options['deadline'] ?? '';
+
+    // php todo.php --deadline "2019-08-30 10:00:00" add "Поздравить маму с днём мамы"
+
+    var_dump($task_text, $deadline);
+
 } else {
     echo red_line("[!!!] unknown command {$command}");
     echo $help;
